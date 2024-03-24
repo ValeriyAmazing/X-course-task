8a87dd12f1ee2d7ca28b6b19b05a00cb24ce0f61
Valeriy K. <vall.sparazzo@gmail.com>
2024-03-24 13:23:10 +0200

    first deploy

---
+++ gulpfile.js
@@ -0,0 +1,92 @@
+const gulp = require("gulp");
+const webp = require("gulp-webp");
+const pug = require("gulp-pug");
+const connect = require("gulp-connect");
+const sass = require("gulp-sass")(require("sass"));
+const del = require("del");
+
+gulp.task("clean-dist", function () {
+  return del(["dist/**/*"]); 
+});
+
+gulp.task("convert-images", function () {
+  return gulp
+    .src("./src/**/*.{png,jpg,jpeg,webp}")
+    .pipe(webp())
+    .pipe(gulp.dest("dist"));
+});
+
+gulp.task("compile-pug", function () {
+  return gulp
+    .src("./src/**/index.pug")
+    .pipe(pug({ pretty: true }))
+    .pipe(gulp.dest("dist"))
+    .pipe(connect.reload());
+});
+
+
+gulp.task("compile-sass", function () {
+  return gulp
+    .src(["./src/**/style.scss"])
+    .pipe(sass({ outputStyle: "expanded" }))
+    .pipe(gulp.dest("dist"))
+    .pipe(connect.reload());
+});
+
+gulp.task("move-svg", function () {
+  return gulp
+    .src(["./src/**/*.svg"]) 
+    .pipe(gulp.dest("dist"));
+});
+
+// gulp.task("move-css", function () {
+//     return gulp
+//       .src(["./src/**/style.css"]) 
+//       .pipe(gulp.dest("dist"));
+//   });
+
+// gulp.task("move-fonts", function () {
+//   return gulp
+//     .src("./src/**/*.{woff,woff2,ttf}") 
+//     .pipe(gulp.dest("dist")); 
+// });
+
+// gulp.task("move-scripts", function () {
+//     return gulp
+//       .src("./src/**/*.js") 
+//       .pipe(gulp.dest("dist")); 
+//   });
+
+gulp.task("watch", function (done) {
+  gulp.watch("src/**/*.pug", gulp.series("compile-pug"));
+  gulp.watch("src/**/*.{jpeg,jpg,png,webp}", gulp.series("convert-images"));
+  gulp.watch("src/**/*.svg", gulp.series("move-svg"));
+  gulp.watch("src/**/*.scss", gulp.series("compile-sass"));
+//   gulp.watch("src/**/*.{woff,woff2,ttf}", gulp.series("move-fonts"));
+//   gulp.watch("src/**/*.js", gulp.series("move-scripts"));
+  // gulp.watch("src/**/*.css", gulp.series("move-css"));
+  done()
+});
+
+gulp.task("connect", function (done) {
+  connect.server({
+    root: "dist",
+    livereload: true,
+  });
+  done()
+});
+
+gulp.task(
+  "dev",
+  gulp.series(
+    "clean-dist",
+    "move-svg",
+    "convert-images",
+    "compile-pug",
+    "compile-sass",
+    // "move-fonts",
+    // "move-scripts",
+    // "move-css",
+    gulp.parallel("watch", "connect")
+  )
+);
