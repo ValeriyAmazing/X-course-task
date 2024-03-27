c96bbcf88781ae7b93cc028199f51b2b290bb19b
Valeriy K. <vall.sparazzo@gmail.com>
2024-03-27 15:49:59 +0200

    updated link in empty_cart page

--- docs/pages/cart/mian.js
+++
@@ -1,7 +0,0 @@
-const items = document.querySelectorAll('.items')
-items.forEach(item => {
-    const btn = item.querySelector('button')
-    btn.addEventListener('click', ()=>{
-        item.remove()
-    })
-})
